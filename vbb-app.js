4839258af8ed992e63a111b18cbc9381e1f63533
Werner Faust <docfaust75@gmail.com>
2020-08-09 23:45:40 +0200

    dynamic port

--- vbb-app.js
+++ vbb-app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -14,6 +16,6 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
-  console.log("Listening to 3000");
+app.listen(port, () => {
+  console.log("Listening to " + port);
 });
